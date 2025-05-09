90dd82fbe63a16fb7811296b5c1a7f3c5095644c
jobinkvarghese <jobinkvarghese7@gmail.com>
2025-05-09 20:46:31 +0530

    add modules

--- NovaCart/NovaCart/settings.py
+++ NovaCart/NovaCart/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Customers_App',
+    'Orders_App',
+    'Products_App',
+    'Themes_App',
 ]
 
 MIDDLEWARE = [
